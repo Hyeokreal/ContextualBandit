24be9abcc8bdacfedb9b9a38b81cba685da24e8e
Hyeokreal <Hyeokryeol.yang@gmail.com>
2018-03-16 16:11:56 +0900

    comment

--- ContextualBandit.py
+++ ContextualBandit.py
@@ -4,6 +4,8 @@ import numpy as np
 class ContextualBandit:
     def __init__(self):
         self.state = 0
+        # gaussian 에서 뽑은 숫자 보다 작아야 reward가 1이기 때문에 각 bandit에서 숫자가 가장 작은 index의 arm을
+        # 당기는 것이 최적입니다.
         self.bandits = np.array([[0.2, 0., 0., -5], [0.1, -5, 1., 0.25], [-5., 5., 5., 5.]])
         self.num_bandits = np.shape(self.bandits)[0]
         self.num_actions = np.shape(self.bandits)[1]
@@ -28,4 +30,3 @@ if __name__ == "__main__":
     print("The number of bandits", cb.num_bandits)
     print("The number of actions", cb.num_actions)
     print("chosen bandit :", cb.get_bandit())
-
